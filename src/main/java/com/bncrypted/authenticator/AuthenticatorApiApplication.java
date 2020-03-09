802a9aa608b52a3a6c8da7b530c54e78fbbd9a7f
bncrypted <bmdfgh.b@gmail.com>
2020-03-09 13:36:09 +0000

    project skeleton with root endpoint

---
+++ src/main/java/com/bncrypted/authenticator/AuthenticatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.bncrypted.authenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticatorApiApplication.class, args);
+	}
+
+}
