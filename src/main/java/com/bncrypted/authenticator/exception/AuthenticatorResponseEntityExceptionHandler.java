d61e39ff56e11d368a38634f8ab55fad80fd7ca9
bncrypted <bmdfgh.b@gmail.com>
2020-03-27 16:44:03 +0000

    exception handler tweaks

--- src/main/java/com/bncrypted/authenticator/exception/AuthenticatorResponseEntityExceptionHandler.java
+++ src/main/java/com/bncrypted/authenticator/exception/AuthenticatorResponseEntityExceptionHandler.java
@@ -9,13 +9,14 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 
 @ControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
-public class AuthenticatorResponseEntityExceptionHandler {
+public class AuthenticatorResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ResponseBody
     @ExceptionHandler(InvalidCredentialsException.class)
@@ -25,19 +26,26 @@ public class AuthenticatorResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
     }
 
-//    @ResponseBody
-//    @ExceptionHandler({ IllegalArgumentException.class, IllegalStateException.class, RuntimeException.class })
-//    public ResponseEntity<ErrorResponse> handleInternalServerErrors(RuntimeException ex, WebRequest request) {
-//        ErrorResponse errorResponse = new ErrorResponse(
-//                HttpStatus.INTERNAL_SERVER_ERROR, ex.getLocalizedMessage(), getCurrentTimestamp());
-//        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
+    @ResponseBody
+    @ExceptionHandler({ IllegalArgumentException.class, IllegalStateException.class, RuntimeException.class })
+    public ResponseEntity<ErrorResponse> handleInternalServerErrors(RuntimeException ex, WebRequest request) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                HttpStatus.INTERNAL_SERVER_ERROR, getRootException(ex).getLocalizedMessage(), getCurrentTimestamp());
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
     private String getCurrentTimestamp() {
         return Instant.now()
-                .truncatedTo(ChronoUnit.SECONDS)
-                .toString()
-                .replaceAll("[TZ]", " ");
+                .truncatedTo(ChronoUnit.MILLIS)
+                .toString();
+    }
+
+    private static Throwable getRootException(Throwable ex) {
+        Throwable root = ex;
+        while (root.getCause() != null) {
+            root = root.getCause();
+        }
+        return root;
     }
 
 }
